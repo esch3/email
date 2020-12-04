24a91c24c644daabda4a7cf964e5d5d316ffdd9b
r4don <compuradon@gmail.com>
2020-12-04 13:34:32 -0500

    added animation feature to email archive

--- mail/mail/mail/static/mail/inbox.js
+++ mail/mail/mail/static/mail/inbox.js
@@ -108,35 +108,30 @@ function displayEmails(emails, mailbox) {
   const user = document.querySelector('#user').textContent;
   let content = document.querySelector('#emails-view');
   let body = document.querySelector('body');
-  body.style.backgroundColor = "white";
-  body.style.fontFamily = "'Padauk', sans-serif";
-  body.style.fontSize = "20px";
-  content.classList.add("container");
+  content.className = "container";
+
   emails.forEach(email => {
     // container for each email
     let emailDiv = document.createElement('div');
     // Set id for each email div to the appropriate ID
     emailDiv.id = email.id;
+    emailDiv.classList.add("row", "email-div");
     
-    // Use bootstrap grid for display
-    emailDiv.classList.add("row");
-    emailDiv.style.height = "75px";
-    emailDiv.style.padding = "10px";
     if (email.read === true) {
       emailDiv.style.backgroundColor = "rgb(220, 220, 220, 0.5)";
     } else {
       emailDiv.style.backgroundColor = "white";
     }
-    emailDiv.style.border = "1px solid rgb(0,0,0,0.1)";
+    
     let senderDiv = document.createElement('div');
     let subjectDiv = document.createElement('div');
     let senderSenderDiv = document.createElement('div');
     let senderDateDiv = document.createElement('div');
-    emailDiv.classList.add("row");
+    
     senderDiv.classList.add("col-4");
     senderSenderDiv.classList.add("row");
     senderDateDiv.classList.add("row");
-    subjectDiv.classList.add("col-8");
+    subjectDiv.classList.add("col-4");
     sender = document.createTextNode(email.sender);
     senderBold = document.createElement('b');
     senderBold.appendChild(sender);
@@ -150,6 +145,40 @@ function displayEmails(emails, mailbox) {
     subjectDiv.appendChild(subject);
     emailDiv.appendChild(senderDiv);
     emailDiv.appendChild(subjectDiv);
+    let archiveDiv = document.createElement('div');
+    archiveDiv.classList.add("col-4");
+    
+    let archiveButton = document.createElement("input");
+    archiveButton.type = "button";
+    archiveButton.value = "Archive";
+    archiveButton.id = "hide";
+    archiveButton.classList.add("btn", "btn-sm", "btn-secondary", "float-right");
+    const archive = () => fetch(`/emails/${email.id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        archived: true
+      })
+    });
+    
+    archiveButton.onclick = function (event) {
+      archive();
+      const element = event.target;
+      
+      const rmv = function() {
+        element.parentNode.parentNode.remove();
+      }
+      event.stopPropagation();
+      if (element.id === 'hide') {
+        element.parentNode.parentNode.style.animationPlayState = 'running';
+        element.parentNode.parentNode.addEventList;
+        setTimeout(rmv, 2500);
+      } 
+    };
+
+    archiveDiv.appendChild(archiveButton);
+    if (!email.archived) {
+      emailDiv.appendChild(archiveDiv);
+    }
     content.appendChild(emailDiv);
     
     // Event listeners
